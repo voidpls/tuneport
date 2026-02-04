51725694de25d2f72ffc62b134d6cf1e22e8354a
Void <voidpls@protonmail.com>
2026-02-03 22:02:01 -0500

    Initial commit: all basic functionality

---
+++ bot.js
@@ -0,0 +1,60 @@
+import 'dotenv/config'
+import { Client, Events, GatewayIntentBits } from 'discord.js'
+import { default as urlRegex } from 'url-regex-safe'
+import { portLink } from './porter.js'
+import strict from 'node:assert/strict'
+const { BOT_TOKEN } = process.env
+
+// minimal regex for detecting music service links
+const musicUrlRegex = {
+  spotify: /(open\.spotify|spotify\.link)/i,
+  youtubeMusic: /music\.youtube\.com/i,
+  apple: /music\.apple\.com/i,
+  soundcloud: /soundcloud\.com/i,
+  tidal: /tidal\.com/i,
+  deezer: /(deezer\.com|deezer\.page\.link)/i,
+  bandcamp: /bandcamp\.com/i
+}
+// should update names to match songlink docs
+
+const bot = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+})
+
+bot.once(Events.ClientReady, readyClient => {
+  console.log(`Logged in as ${readyClient.user.username}`)
+})
+
+bot.on(Events.MessageCreate, async msg => {
+  if (msg.author.bot || msg.author.id === bot.user.id) return
+
+  const links = msg.content.match(urlRegex({ strict: true }))
+  if (!links) return
+  const streamingLinks = matchSongLinks(links)
+  await portLink(bot, msg, streamingLinks)
+})
+
+// function: match all links against specified streaming platforms
+// return: matched links without duplicates
+function matchSongLinks (links) {
+  const streamingLinks = links
+    .map(url => {
+      const platformsToMatch = Object.keys(musicUrlRegex)
+      // .find on keys to get streaming service name as return argument
+      const found = platformsToMatch.find(k => url.match(musicUrlRegex[k]))
+      if (found) return { platform: found, url }
+      return null
+    })
+    .filter(obj => obj) // remove nulls
+  // use Map to remove duplicate values
+  const uniqStreamingLinks = [
+    ...new Map(streamingLinks.map(item => [item.id, item])).values()
+  ]
+  return uniqStreamingLinks
+}
+
+bot.login(BOT_TOKEN)
---
+++ porter.js
@@ -0,0 +1,46 @@
+import { EmbedBuilder } from 'discord.js'
+import platformMapping from './platformMapping.json' with {type: 'json'}
+const enabledPlatforms = process.env.ENABLED_PLATFORMS.split(',')
+
+export async function portLink (bot, msg, streamingLinks) {
+  // console.log(streamingLinks)
+  // limit number of links per message (3), as it will convert all links
+  if (streamingLinks.length > 3) streamingLinks.length = 3
+  for (const streamingLink of streamingLinks) {
+    const url = streamingLink.url
+
+    const endpoint = `https://api.song.link/v1-alpha.1/links?url=${url}&songIfSingle=true`
+    const options = { method: 'GET' }
+
+    const res = await fetch(endpoint, options)
+    // if failed to fetch, just return
+    if (!res.ok) return console.log(res.status, res.statusText, streamingLink)
+    const data = await res.json()
+    // console.log(data)
+    if (data.entitiesByUniqueId.length === 1) return // return if no other results
+    const metadata = data.entitiesByUniqueId[data.entityUniqueId]
+
+    // console.log(metadata)
+    sendMessage(msg, data, metadata, streamingLink.platform)
+  }
+}
+
+async function sendMessage (msg, data, metadata, originalPlatform) {
+  let desc = ''
+  enabledPlatforms.forEach(platform => {
+    if (originalPlatform === platform) return // return if same as original platform
+    if (data.linksByPlatform[platform]) {
+      const platformName = platformMapping[platform]
+      const url = data.linksByPlatform[platform].url
+      desc += `**[${platformName}](${url})**\n`
+    }
+  })
+
+  const embed = new EmbedBuilder()
+    .setColor(0x313338)
+    .setDescription(`**${metadata.artistName} - ${metadata.title}**\n\n${desc}`)
+    .setThumbnail(metadata.thumbnailUrl)
+    .setFooter({text: `Powered by Songlink/Odesli`})
+
+  msg.reply({embeds: [embed], allowedMentions: { repliedUser: false }})
+}
